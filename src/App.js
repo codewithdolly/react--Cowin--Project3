36ac0f58b70ffaf67e68e079ad68c065e133e8d3
hicoderindia <hicoder.dolly@gmail.com>
2021-10-21 14:07:48 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.scss";
+import Home from "./05-10-2021/Code/Components/Home/Home";
+import AttendanceSheet from './28-09-2021/Code/Components/AttendanceSheet/AttendanceSheet'
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <AttendanceSheet />
+    </div>
+  );
+};
+
+export default App;
